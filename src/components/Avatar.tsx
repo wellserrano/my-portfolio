dd85e8cb5e55b4d7042b506579f9326237830fc6
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-02-27 16:45:49 -0300

    fix: avatar reworked using Radix UI

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,9 +1,20 @@
 import avatarPicture from '../assets/avatar.png'
+import * as AvatarReact from '@radix-ui/react-avatar';
 
 export function Avatar() {
   return (
-    <div className="w-32 h-32 rounded-full border-4 border-[#FFE071] p-1 mb-10">
-      <img src={ avatarPicture } alt="" />
-    </div>
+    <AvatarReact.Root className="w-32 h-32 rounded-full border-4 border-[#FFE071] p-1 mb-10">
+       <AvatarReact.Image
+        className="h-full w-full rounded-[inherit] object-cover"
+        src={ avatarPicture }
+        alt="my profile picture"
+      />
+      <AvatarReact.Fallback
+        className="text-violet11 leading-1 flex h-full w-full rounded-[inherit] items-center justify-center bg-[#FFE071] text-[15px] font-medium"
+        delayMs={600}
+      >
+        WS
+      </AvatarReact.Fallback>
+    </AvatarReact.Root>
   )
 }
