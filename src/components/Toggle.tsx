fe195d336b2678568e1bea02bf4f175bb0a410cb
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-02-28 20:14:44 -0300

    Toggle responsive breakpoint added

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -3,11 +3,11 @@ import { useState } from 'react';
 
 export function Toggle() {
   const [value, setValue] = useState('portfolio');
-  const toggleItemStyle = 'uppercase flex items-center justify-center w-36 h-12 rounded-[inherit] data-[state=on]:bg-[#D7D7D7] dark:data-[state=on]:bg-[#0C151D] text-[#19191B] dark:text-[#A3ABB2] focus:outline-none data-[state=on]:animate-popIn data-[state=off]:animate-popOut'
+  const toggleItemStyle = 'uppercase flex items-center justify-center w-36 h-12 sm:w-[22rem] sm:h-[4rem] rounded-[inherit] data-[state=on]:bg-[#D7D7D7] dark:data-[state=on]:bg-[#0C151D] text-[#19191B] dark:text-[#A3ABB2] focus:outline-none data-[state=on]:animate-popIn data-[state=off]:animate-popOut'
     
   return (
     <ToggleGroup.Root
-      className="flex justify-between items-center w-72 h-16 bg-white dark:bg-[#171F26] rounded-xl p-2"
+      className="flex justify-around items-center w-72 h-16 sm:w-[28rem] sm:h-[6rem] bg-white dark:bg-[#171F26] rounded-xl p-2 gap-4 text-sm sm:text-lg"
       type="single"
       value={value}
       onValueChange={(value) => {
