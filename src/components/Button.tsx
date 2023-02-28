2a3f2a05d0dd3ee3bd5228301ef76a1f8366fcf4
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-02-28 20:14:01 -0300

    fix: text size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ export function Button({ title, children }: Props ) {
   return (
       <button 
         type="button"
-        className="flex justify-center items-center gap-2 w-36 h-11 sm:w-72 sm:h-20 bg-[#FFE071] text-[#3D3D3D] rounded-xl hover:opacity-90 transition-opacity"
+        className="flex justify-center items-center gap-2 w-36 h-11 sm:w-72 sm:h-20 bg-[#FFE071] text-[#3D3D3D] text-sm sm:text-2xl rounded-xl hover:opacity-90 transition-opacity"
       >
         { title }
         { children }
