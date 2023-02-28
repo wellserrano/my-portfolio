36c52008218b85e21621e5bdc0838618507e73e5
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-02-28 10:39:50 -0300

    ProjectCard props

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,7 +6,13 @@ import { Browser, GithubLogo } from 'phosphor-react'
 
 import { useState } from 'react';
 
-export function ProjectCard() {
+interface Props {
+  title: string
+  description: string
+  image: string
+}
+
+export function ProjectCard({ title, description, image }: Props) {
   const [open, setOpen] = useState(false);
 
   return (
@@ -20,48 +26,53 @@ export function ProjectCard() {
         <AspectRatio.Root ratio={16 / 9}>
           <img
             className="h-full w-full object-cover opacity-80 hover:opacity-100"
-            src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
-            alt="Landscape photograph by Tobias Tullius"
+            src={ image }
+            alt="webpage image"
           />
         </AspectRatio.Root>
       </Collapsible.Trigger>
 
       <Collapsible.Content>
         <div className='flex justify-around py-6'>
-        <Tooltip.Provider delayDuration={800}>
-          <Tooltip.Root>
-            <Tooltip.Trigger asChild>
-              <Browser size={36} color='#A3ABB2' className='hover:cursor-pointer' />
-            </Tooltip.Trigger>
-            <Tooltip.Portal>
-              <Tooltip.Content
-                className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
-                sideOffset={5}
-                side='bottom'
-              >
-                Visit website
-                <Tooltip.Arrow className="fill-white" />
-              </Tooltip.Content>
-            </Tooltip.Portal>
-          </Tooltip.Root>
-        </Tooltip.Provider>
-        <Tooltip.Provider>
-          <Tooltip.Root>
-            <Tooltip.Trigger asChild>
-              <GithubLogo size={36} color='#A3ABB2' className='hover:cursor-pointer' />
-            </Tooltip.Trigger>
-            <Tooltip.Portal>
-              <Tooltip.Content
-                className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
-                sideOffset={5}
-                side='bottom'
-              >
-                Go to github
-                <Tooltip.Arrow className="fill-white" />
-              </Tooltip.Content>
-            </Tooltip.Portal>
-          </Tooltip.Root>
-        </Tooltip.Provider>
+          <h2>{ title }</h2>
+          <p>{ description }</p>
+
+          <Tooltip.Provider delayDuration={800}>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <Browser size={36} color='#A3ABB2' className='hover:cursor-pointer' />
+              </Tooltip.Trigger>
+              <Tooltip.Portal>
+                <Tooltip.Content
+                  className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
+                  sideOffset={5}
+                  side='bottom'
+                >
+                  Visit website
+                  <Tooltip.Arrow className="fill-white" />
+                </Tooltip.Content>
+              </Tooltip.Portal>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+
+          <Tooltip.Provider>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <GithubLogo size={36} color='#A3ABB2' className='hover:cursor-pointer' />
+              </Tooltip.Trigger>
+              <Tooltip.Portal>
+                <Tooltip.Content
+                  className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
+                  sideOffset={5}
+                  side='bottom'
+                >
+                  Go to github
+                  <Tooltip.Arrow className="fill-white" />
+                </Tooltip.Content>
+              </Tooltip.Portal>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+
         </div>
       </Collapsible.Content>
 
