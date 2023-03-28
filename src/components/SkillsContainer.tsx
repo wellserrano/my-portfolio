38f391182e2d7b4b7b3ba73e6611819ef4798c71
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-03-28 16:00:31 -0300

    feat: displaying skills

--- src/components/SkillsContainer.tsx
+++ src/components/SkillsContainer.tsx
@@ -1,9 +1,31 @@
+import * as Collapsible from '@radix-ui/react-collapsible';
+
 import { SkillCard } from "./SkillCard"
 
+import tailwindcss from '../assets/skills/tailwindcss.svg'
+import javascript from '../assets/skills/javascript.svg'
+import typescript from '../assets/skills/typescript.svg'
+import python from '../assets/skills/python.svg'
+import delphi from '../assets/skills/delphi.svg'
+import react from '../assets/skills/react.svg'
+import next from '../assets/skills/next.svg'
+import fastify from '../assets/skills/fastify.svg'
+import prisma from '../assets/skills/prisma.svg'
+import node from '../assets/skills/node.svg'
+
+import { CaretDown, CaretUp } from 'phosphor-react';
+import { useState } from 'react';
+
+
 export function SkillsContainer() {
+  const [languageOpen, setLanguageOpen] = useState(true)
+  const [frameworkOpen, setFrameworkOpen] = useState(true)
+  const [librarieOpen, setLibrarieOpen] = useState(true)
+
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 justify-items-center justify-center items-center gap-8">
+
         <div className="w-96 text-justify">
           <p className="text-lg leading-relaxed text-[#3D3D3D] dark:text-[#F1F2F4]">
             I am <strong>Wellington Serrano</strong>, a professional web developer with over 3 years of experience in creating dynamic and responsive web/desktop applications.
@@ -15,9 +37,51 @@ export function SkillsContainer() {
             <br /><br />&there4; Up-to-date with the latest trends and technologies in the industry to ensure my clients receive cutting-edge web solutions.
           </p>
         </div>
-        <div className="w-72 h-full">
-        <SkillCard skill="Tailwind CSS" level={100}/>
+
+        <div className="w-72 h-full ml-14">
+
+          <Collapsible.Root open={languageOpen} onOpenChange={setLanguageOpen}>
+          <Collapsible.Trigger asChild>
+            <p className='flex flex-row justify-center items-center gap-2 text-xl font-bold text-white mb-6 cursor-pointer hover:opacity-75'>
+              Programming Languages {languageOpen ? <CaretUp /> : <CaretDown />}
+            </p>
+          </Collapsible.Trigger>
+          <Collapsible.Content>
+            <SkillCard skill="NodeJS" level={80} image={node} />
+            <SkillCard skill="JavaScript" level={80} image={javascript} />
+            <SkillCard skill="TypeScript" level={65} image={typescript} />
+            <SkillCard skill="Python" level={60} image={python} />
+            <SkillCard skill="Delphi" level={75} image={delphi} />
+          </Collapsible.Content>
+          </Collapsible.Root>
+
+          <Collapsible.Root open={frameworkOpen} onOpenChange={setFrameworkOpen}>
+          <Collapsible.Trigger asChild>
+            <p className='flex flex-row justify-center items-center gap-2 text-xl font-bold text-white mb-6 cursor-pointer hover:opacity-75'>
+              Frameworks {frameworkOpen ? <CaretUp /> : <CaretDown />}
+            </p>
+          </Collapsible.Trigger>
+          <Collapsible.Content>
+            <SkillCard skill="React" level={75} image={react} />
+            <SkillCard skill="React Native" level={75} image={react} />
+            <SkillCard skill="NextJS" level={55} image={next} />
+          </Collapsible.Content>
+          </Collapsible.Root>
+
+          <Collapsible.Root open={librarieOpen} onOpenChange={setLibrarieOpen}>
+          <Collapsible.Trigger asChild>
+            <p className='flex flex-row justify-center items-center gap-2 text-xl font-bold text-white mb-6 cursor-pointer hover:opacity-75'>
+              Libraries {librarieOpen ? <CaretUp /> : <CaretDown />}
+            </p>
+          </Collapsible.Trigger>
+          <Collapsible.Content>
+            <SkillCard skill="Tailwind CSS" level={75} image={tailwindcss} />
+            <SkillCard skill="Fastify" level={65} image={fastify} />
+            <SkillCard skill="Prisma" level={80} image={prisma} />
+          </Collapsible.Content>
+          </Collapsible.Root>
         </div>
+
       </div>
     </>
   )
