0bf9ac37cfbd76080a5c90a500992d318316d6a7
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-03-28 14:57:31 -0300

    perf: data separated from rendering objects

--- src/components/ProjectsContainer.tsx
+++ src/components/ProjectsContainer.tsx
@@ -1,41 +1,24 @@
 import { ProjectCard } from "./ProjectCard"
-
-import pacmanImage from '../assets/projects-images/pacman.png'
-import dashboardImage from '../assets/projects-images/dashboard.png'
-import soroblocoImage from '../assets/projects-images/sorobloco.png'
-import rocketmoviesImage from '../assets/projects-images/rocketmovies.png'
+import { projectsData } from '../data/projects'
 
 export function ProjectsContainer() {
   return (
-      <div className='grid grid-cols-1 sm:grid-cols-2 justify-items-center justify-center items-center gap-8'>
-        <ProjectCard 
-          title='Dashboard' 
-          description='Simple dashboard design shows specific data'
-          image={ dashboardImage }
-          githubLink='https://github.com/wellserrano/dashboard'
-          siteLink='https://wellserrano.github.io/dashboard/'
-        />
-        <ProjectCard 
-          title='Sorobloco' 
-          description='Display street carnival groups in Sorocaba'
-          image={ soroblocoImage }
-          githubLink='https://github.com/wellserrano/sorobloco'
-          siteLink='https://sorobloco.netlify.app'
-        />
-        <ProjectCard 
-          title='RocketMovies' 
-          description='Platform dedicated to organize your favorite movies'
-          image={ rocketmoviesImage }
-          githubLink='https://github.com/wellserrano/rocketMovies-app'
-          siteLink='https://app-rocket-movies.netlify.app'
-        />
-        <ProjectCard 
-          title='Pacman' 
-          description='Vanilla pacman implementation'
-          image={ pacmanImage }
-          githubLink='https://github.com/wellserrano/pacman'
-          siteLink='https://wellserrano.github.io/pacman/'
-        />
-      </div>
+    <div className='grid grid-cols-1 sm:grid-cols-2 justify-items-center justify-center items-center gap-8'>
+      {
+        projectsData &&
+        projectsData.map((project, i) => {
+          return (
+            <ProjectCard 
+              key={project.title+i}
+              title={project.title} 
+              description={project.description}
+              image={ project.image }
+              githubLink={ project.githubLink }
+              siteLink={ project.siteLink }
+            />
+          )
+        })
+      }
+    </div>
   ) 
 }
---
+++ src/data/projects.ts
@@ -0,0 +1,35 @@
+import pacmanImage from '../assets/projects-images/pacman.png'
+import dashboardImage from '../assets/projects-images/dashboard.png'
+import soroblocoImage from '../assets/projects-images/sorobloco.png'
+import rocketmoviesImage from '../assets/projects-images/rocketmovies.png'
+
+export const projectsData = [
+  {
+    title: 'Dashboard',
+    description: 'Simple dashboard design shows specific data',
+    image: dashboardImage,
+    githubLink: 'https://github.com/wellserrano/dashboard',
+    siteLink: 'https://wellserrano.github.io/dashboard/',
+  },
+  {  
+    title: 'Sorobloco',
+    description: 'Display street carnival groups in Sorocaba',
+    image: soroblocoImage,
+    githubLink: 'https://github.com/wellserrano/sorobloco',
+    siteLink: 'https://sorobloco.netlify.app',
+  },
+  {
+    title: 'RocketMovies',
+    description: 'Platform dedicated to organize your favorite movies',
+    image: rocketmoviesImage,
+    githubLink: 'https://github.com/wellserrano/rocketMovies-app',
+    siteLink: 'https://app-rocket-movies.netlify.app',
+  },
+  {
+    title: 'Pacman',
+    description: 'Vanilla pacman implementation',
+    image: pacmanImage,
+    githubLink: 'https://github.com/wellserrano/pacman',
+    siteLink: 'https://wellserrano.github.io/pacman/',
+  },
+]
