eea5bc15798e7258bdf13e716e7bd633128a1811
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-03-28 13:07:33 -0300

    feat: toggling renders different info

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,18 +1,19 @@
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
-import { useState } from 'react';
 
-export function Toggle() {
-  const [value, setValue] = useState('portfolio');
+interface ToggleProps {
+  activeSection: string
+  handleToggle: (value:string) => void
+}
+
+export function Toggle({activeSection, handleToggle} :ToggleProps) {
   const toggleItemStyle = 'uppercase flex items-center justify-center w-36 h-12 sm:w-[22rem] sm:h-[4rem] rounded-[inherit] data-[state=on]:bg-[#D7D7D7] dark:data-[state=on]:bg-[#0C151D] text-[#19191B] dark:text-[#A3ABB2] focus:outline-none dark:data-[state=on]:animate-popIn dark:data-[state=off]:animate-popOut'
     
   return (
     <ToggleGroup.Root
       className="flex justify-around items-center w-72 h-16 sm:w-[28rem] sm:h-[6rem] bg-white dark:bg-[#171F26] rounded-xl p-2 gap-4 text-sm sm:text-lg"
       type="single"
-      value={value}
-      onValueChange={(value) => {
-        if (value) setValue(value);
-      }}
+      value={ activeSection }
+      onValueChange={(value) => handleToggle(value) }
     >
       <ToggleGroup.Item
         className={ toggleItemStyle }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,8 +13,14 @@ import pacmanImage from '../assets/projects-images/pacman.png'
 import dashboardImage from '../assets/projects-images/dashboard.png'
 import soroblocoImage from '../assets/projects-images/sorobloco.png'
 import rocketmoviesImage from '../assets/projects-images/rocketmovies.png'
+import { useState } from "react"
 
 export function Home() {
+  const [toggleValue, setToggleValue] = useState<string>('portfolio')
+
+  function handleToggle(e:string) {
+    setToggleValue(e)
+  }
 
   return (
     <div className="flex flex-col min-h-screen justify-center items-center pt-5 px-5 pb-10 dark:bg-[#0C151D] bg-[#E5E5E5] transition-colors ease-out attach">
@@ -47,39 +53,65 @@ export function Home() {
         </div>
 
         <div className='flex justify-center items-center mb-8'>
-          <Toggle />
+          <Toggle activeSection={ toggleValue } handleToggle={ handleToggle }/>
         </div>
 
-        <div className='grid grid-cols-1 sm:grid-cols-2 justify-items-center justify-center items-center gap-8'>
-          <ProjectCard 
-            title='Dashboard' 
-            description='Simple dashboard design shows specific data'
-            image={ dashboardImage }
-            githubLink='https://github.com/wellserrano/dashboard'
-            siteLink='https://wellserrano.github.io/dashboard/'
-          />
-          <ProjectCard 
-            title='Sorobloco' 
-            description='Display street carnival groups in Sorocaba'
-            image={ soroblocoImage }
-            githubLink='https://github.com/wellserrano/sorobloco'
-            siteLink='https://sorobloco.netlify.app'
-          />
-          <ProjectCard 
-            title='RocketMovies' 
-            description='Platform dedicated to organize your favorite movies'
-            image={ rocketmoviesImage }
-            githubLink='https://github.com/wellserrano/rocketMovies-app'
-            siteLink='https://app-rocket-movies.netlify.app'
-          />
-          <ProjectCard 
-            title='Pacman' 
-            description='Vanilla pacman implementation'
-            image={ pacmanImage }
-            githubLink='https://github.com/wellserrano/pacman'
-            siteLink='https://wellserrano.github.io/pacman/'
-          />
-        </div>
+        {
+          toggleValue === 'portfolio' ?
+          <>
+            <div className='grid grid-cols-1 sm:grid-cols-2 justify-items-center justify-center items-center gap-8'>
+              <ProjectCard 
+                title='Dashboard' 
+                description='Simple dashboard design shows specific data'
+                image={ dashboardImage }
+                githubLink='https://github.com/wellserrano/dashboard'
+                siteLink='https://wellserrano.github.io/dashboard/'
+              />
+              <ProjectCard 
+                title='Sorobloco' 
+                description='Display street carnival groups in Sorocaba'
+                image={ soroblocoImage }
+                githubLink='https://github.com/wellserrano/sorobloco'
+                siteLink='https://sorobloco.netlify.app'
+              />
+              <ProjectCard 
+                title='RocketMovies' 
+                description='Platform dedicated to organize your favorite movies'
+                image={ rocketmoviesImage }
+                githubLink='https://github.com/wellserrano/rocketMovies-app'
+                siteLink='https://app-rocket-movies.netlify.app'
+              />
+              <ProjectCard 
+                title='Pacman' 
+                description='Vanilla pacman implementation'
+                image={ pacmanImage }
+                githubLink='https://github.com/wellserrano/pacman'
+                siteLink='https://wellserrano.github.io/pacman/'
+              />
+            </div>
+          </>
+
+          : 
+          <>
+            <div className="grid grid-cols-1 sm:grid-cols-2 justify-items-center justify-center items-center gap-8">
+              <div className="w-96 text-start">
+                <p className="text-lg leading-relaxed text-[#3D3D3D] dark:text-[#F1F2F4]">
+                  I am <strong>Wellington Serrano</strong>, a professional web developer with over 3 years of experience in creating dynamic and responsive web/desktop applications.
+                  My portfolio website showcases my expertise in crafting user-friendly and visually appealing web solutions that cater to a wide range of industries and target audiences.
+                  I have honed my skills in <strong className="italic">HTML, CSS, JavaScript, TypeScript</strong> and various web frameworks, tools and technologies such as <strong className="text-lg text-[#FFE071]">React, NextJS, React Native, Expo</strong> and <strong className="text-[#FFE071]">NodeJS</strong>. 
+                  <br /><br />I am a problem-solver at heart, who enjoys tackling complex challenges to develop innovative and creative solutions that drive business growth and success.
+                  <br /><br />&there4; Committed to delivering high-quality work that meets the unique needs and requirements of my clients. 
+                  <br /><br />&there4; Keen eye for detail and strive to exceed expectations. 
+                  <br /><br />&there4; Up-to-date with the latest trends and technologies in the industry to ensure my clients receive cutting-edge web solutions.
+                </p>
+              </div>
+              <div className="w-72 h-full">
+
+              </div>
+            </div>
+          </>
+        }
+        
 
       </main>
       <footer className='flex justify-center items-center text-center text-sm sm:text-base text-[#3D3D3D] dark:text-[#F1F2F4] font-normal mt-12'>
