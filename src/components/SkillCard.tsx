d5d3337c097917bddd366ec5e159ec6146a4f651
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-03-28 13:52:08 -0300

    feat: new component SkillCard

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,14 @@
+import * as Progress from '@radix-ui/react-progress';
+
+interface SkillCardProps {
+  language: string
+  level: number
+}
+
+export function SkillCard({ language, level }: SkillCardProps) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
